db2677f9526048c1b26baf0c5be0d0493757f05d
RekGRpth <rekgrpth@gmail.com>
2020-07-02 09:28:42 +0500

    up

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 #docker build --tag rekgrpth/libreoffice .
 #docker push rekgrpth/libreoffice
 #docker pull rekgrpth/libreoffice
-docker volume create pgadmin
+docker volume create libreoffice
 docker network create --attachable --opt com.docker.network.bridge.name=docker docker || echo $?
 docker stop libreoffice || echo $?
 docker rm libreoffice || echo $?
@@ -15,7 +15,7 @@ docker run \
     --env USER_ID=$(id -u) \
     --hostname libreoffice \
     --mount type=bind,source=/etc/certs,destination=/etc/certs,readonly \
-    --mount type=volume,source=pgadmin,destination=/home \
+    --mount type=volume,source=libreoffice,destination=/home \
     --name libreoffice \
     --network docker \
     rekgrpth/libreoffice
