6f4bc254b2c6ac36cd27468c46e5358e6b67c220
RekGRpth <rekgrpth@gmail.com>
2021-10-17 09:46:46 +0500

    edge

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -eux
 
-DOCKER_BUILDKIT=1 docker build --progress=plain --tag rekgrpth/libreoffice . 2>&1 | tee build.log
+DOCKER_BUILDKIT=1 docker build --progress=plain --tag ghcr.io/rekgrpth/libreoffice.docker . 2>&1 | tee build.log
